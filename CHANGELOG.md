80ad3156d5d08b5f352b8f16eaa347db9ffcd441
Mickaël Le Ralec <mickael.leralec@gmail.com>
2020-02-25 19:11:55 +0100

    v 1.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# react-progressbar-on-scroll
+
+### 1.1.0 (2020-02-25)
+
+- Improve performance with ticking
+
+### 1.0.0 (2019-08-27)
+
+- Initial public release
--- rollup.config.js
+++ rollup.config.js
@@ -5,23 +5,21 @@ import typescript from 'rollup-plugin-typescript'
 import { terser } from 'rollup-plugin-terser'
 
 export default {
-    input: 'src/ProgressBar.tsx',
-    output: {
-        file: 'dist/index.js',
-        name: 'index',
-        globals: {
-            react: 'React',
-        },
-        format: 'cjs', // for npm module (use with import/require)
-        // format: 'iife' // for browser (use with React CDN)
-        // format: 'umd' // for both
+  input: 'src/ProgressBar.tsx',
+  output: {
+    file: 'dist/index.js',
+    name: 'index',
+    globals: {
+      react: 'React',
     },
-    plugins: [
-        resolve(),
-        babel({ exclude: 'node_modules/**' }),
-        typescript(),
-        terser(),
-        commonjs(),
-    ],
-    external: ['react'],
+    format: 'cjs',
+  },
+  plugins: [
+    resolve(),
+    babel({ exclude: 'node_modules/**' }),
+    typescript(),
+    terser(),
+    commonjs(),
+  ],
+  external: ['react'],
 }
